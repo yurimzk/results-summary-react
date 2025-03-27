b49087a890fa3e5e667282598d21410246d75c55
Yuri Tagomori <goncalvessyuri@gmail.com>
2025-03-27 15:52:07 -0300

    set up react project with vite:

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './styles/main.scss'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+  </StrictMode>,
+)
