7c9b70027753ebf1851784ebb0ef961cba801801
Yuri Tagomori <goncalvessyuri@gmail.com>
2025-03-31 14:04:26 -0300

    add basic structure and icons

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,9 +1,24 @@
+import { Brain, ChatTeardropDots, Eye, Lightning, LightningA } from '@phosphor-icons/react'
 import './Summary.scss'
 
 export default function Summary () {
   return (
-    <div>
+    <div className='summary-container'>
       <h2>Summary</h2>
+      <div className="summary-list">
+        <div className='reaction-category'>
+          <Lightning />
+        </div>
+        <div className='memory-category'>
+          <Brain />
+        </div>
+        <div className='verbal-category'>
+          <ChatTeardropDots />
+        </div>
+        <div className='visual-category'>
+          <Eye />
+        </div>
+      </div>
     </div>
   )
 }
