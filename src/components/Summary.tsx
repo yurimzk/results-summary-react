4f5a0396872ea2bb44d817194a80fc9516872186
Yuri Tagomori <goncalvessyuri@gmail.com>
2025-03-31 14:49:13 -0300

    change heading and scores style

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,4 +1,4 @@
-import { Brain, ChatTeardropDots, Eye, Lightning, LightningA } from '@phosphor-icons/react'
+import { Brain, ChatTeardropDots, Eye, Lightning } from '@phosphor-icons/react'
 import './Summary.scss'
 
 export default function Summary () {
@@ -8,45 +8,50 @@ export default function Summary () {
       <div className="summary-list">
         <div className='reaction-category'>
           <div className='category-heading'>
-            <Lightning />
-            <h3>Reaction</h3>
+            <Lightning className='reaction-icon' />
+            <h3 className='reaction-heading'>Reaction</h3>
           </div>
           <div className='category-score'>
             <span className='reaction-score'>80</span>
-            <p>/ 100</p>
+            <div>/</div>
+            <p>100</p>
           </div>
         </div>
         <div className='memory-category'>
           <div className='category-heading'>
-            <Brain />
-            <h3>Memory</h3>
+            <Brain className='memory-icon' />
+            <h3 className='memory-heading'>Memory</h3>
           </div>
           <div className='category-score'>
             <span className='memory-score'>92</span>
-            <p>/ 100</p>
+            <div>/</div>
+            <p>100</p>
           </div>
         </div>
         <div className='verbal-category'>
           <div className='category-heading'>
-            <ChatTeardropDots />
-            <h3>Verbal</h3>
+            <ChatTeardropDots className='verbal-icon' />
+            <h3 className='verbal-heading'>Verbal</h3>
           </div>
           <div className='category-score'>
             <span className='verbal-scoree'>61</span>
-            <p>/ 100</p>
+            <div>/</div>
+            <p>100</p>
           </div>
         </div>
         <div className='visual-category'>
           <div className="category-heading">
-            <Eye />
-            <h3>Visual</h3>
+            <Eye className='visual-icon' />
+            <h3 className='visual-heading'>Visual</h3>
           </div>
           <div className='category-score'>
             <span className='visual-score'>73</span>
-            <p>/ 100</p>
+            <div>/</div>
+            <p>100</p>
           </div>
         </div>
       </div>
+      <button className='continue-btn'>Continue</button>
     </div>
   )
 }
