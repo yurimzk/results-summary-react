bb7233bf5e4d941f54f1f59eab0713e1801bdeae
Yuri Tagomori <goncalvessyuri@gmail.com>
2025-03-31 12:02:32 -0300

    add result content

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -2,8 +2,18 @@ import './Result.scss'
 
 export default function Result() {
   return (
-    <div>
-      <h2>Result</h2>
+    <div className='result-container'>
+      <h2>Your Result</h2>
+      <div className="score-container">
+        <div className="score">
+          <span>76</span>
+          <span>of 100</span>
+        </div>
+        <div className="result-text">
+          <h3>Great</h3>
+          <p>You scored higher than 65% of the people who have taken these tests.</p>
+        </div>
+      </div>
     </div>
   )
 }
