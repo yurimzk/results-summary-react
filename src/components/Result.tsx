3bddda2b1230a7a2d70973151f7bc1da2bb75775
Yuri Tagomori <goncalvessyuri@gmail.com>
2025-03-31 13:57:25 -0300

    style results section

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -6,8 +6,8 @@ export default function Result() {
       <h2>Your Result</h2>
       <div className="score-container">
         <div className="score">
-          <span>76</span>
-          <span>of 100</span>
+          <span className='score-number'>76</span>
+          <span className='max-score'>of 100</span>
         </div>
         <div className="result-text">
           <h3>Great</h3>
